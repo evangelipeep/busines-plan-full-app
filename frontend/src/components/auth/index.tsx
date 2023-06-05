bbaf5c6d7d122227456a47645fea610026152237
evangelipeep <evangelipeep@gmail.com>
2023-06-05 23:39:23 +0300

    ProfilePage-update

--- frontend/src/components/auth/index.tsx
+++ frontend/src/components/auth/index.tsx
@@ -2,12 +2,31 @@ import React from 'react'
 import { useLocation } from 'react-router-dom'
 import { Login } from './login/Login'
 import { Register } from './register/Register'
+import { Box } from '@mui/material'
 
 export const AuthRootComponent = () => {
   const location = useLocation()
-  return location.pathname === '/login' ? (
-    <Login />
-  ) : location.pathname === '/register' ? (
-    <Register />
-  ) : null
+  return (
+    <div>
+      <div>
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignContent="center"
+          flexDirection="column"
+          maxWidth={640}
+          margin="auto"
+          padding={5}
+          borderRadius={5}
+          boxShadow="5px 5px 10px #ccc"
+        >
+          {location.pathname === '/login' ? (
+            <Login />
+          ) : location.pathname === '/register' ? (
+            <Register />
+          ) : null}
+        </Box>
+      </div>
+    </div>
+  )
 }
--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -1,18 +1,48 @@
 import React from 'react'
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom'
 import { Login } from '../components/auth/login/Login'
 import { Register } from '../components/auth/register/Register'
 import { Forgot } from '../components/auth/forgot/Forgot'
+import { Button } from '../components/button/Button'
 
-
-export const ProfilePage = () => {
+const ProfilePage: React.FC = () => {
   return (
-    <>
-      <Routes>
-        <Route index element={<Login/>}/> 
-        <Route path='register' element={<Register/>}/>
-        <Route path='forgot' element={<Forgot/>}/>
-      </Routes>
-    </>
+    <div className="flex justify-center items-center h-screen bg-hsl(76, 37%, 68%)">
+      <div className="bg-white p-8 rounded-lg shadow-lg">
+        <div className="flex items-center">
+          <div className="rounded-full w-16 h-16 bg-gray-300"></div>
+          <div className="ml-4">
+            <h2 className="text-2xl font-bold">Имя Пользователя</h2>
+            <div className="flex items-center mt-2">
+              <div className="w-4 h-4 bg-green-500 rounded-full"></div>
+              <p className="text-sm ml-1">Принято</p>
+            </div>
+          </div>
+        </div>
+        <div className="mt-4">
+          <p>Анкета еще не отправлена</p>
+          <Link
+            to="/calculator"
+            className="text-my-green-2 hover:text-black transition underline"
+          >
+            Ссылка на анкету
+          </Link>
+        </div>
+        <div className="mt-4">
+          <textarea
+            className="w-full h-20 p-2 border border-gray-300 rounded"
+            placeholder="Напишите свой пост..."
+          ></textarea>
+          <div className="flex items-center mt-2">
+            <input type="file" className="mr-2" />
+            <button type="submit">
+              <Button isFilled={true} children="Отправить" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
+
+export default ProfilePage
