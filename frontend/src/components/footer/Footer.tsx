bf5b5fc83b0b990b1623a9f78754417a64ec612f
evangelipeep <evangelipeep@gmail.com>
2023-06-19 20:27:47 +0300

    footer

--- frontend/src/components/footer/Footer.tsx
+++ frontend/src/components/footer/Footer.tsx
@@ -2,14 +2,10 @@ import { ReactComponent as GitHub } from '../../assets/svg/github.svg'
 
 export const Footer = () => {
   return (
-    <footer className='bg-my-green-2  h-[60px] flex items-center justify-center'>
-      <span className='text-sm font-bold'>
-        © 2023
-      </span>
-      <a href='https://github.com/evangelipeep' className='hover:underline'>
-        <GitHub
-        
-        className='opacity-100 hover:opacity-80'/>
+    <footer className="bg-my-green-2  h-[60px] flex items-center justify-center">
+      <span className="text-sm font-bold">© 2023</span>
+      <a href="https://github.com/AnastasiiaFalcon" className="hover:underline">
+        <GitHub className="opacity-100 hover:opacity-80" />
       </a>
     </footer>
   )
